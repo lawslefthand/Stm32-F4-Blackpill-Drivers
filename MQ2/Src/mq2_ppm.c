8e49a3b418ce0bd7af3ec016af742856f01af2e9
PUSHKAR <166206853+Pushkar4568@users.noreply.github.com>
2025-08-27 00:18:40 +0530

    Update mq2_ppm.c
    Calibrated Mq2

--- MQ2/Src/mq2_ppm.c
+++ MQ2/Src/mq2_ppm.c
@@ -4,13 +4,14 @@
 #include <mq2_ppm.h>
 #include <stdio.h>
 
-#define VREF     5.0f
-#define ADC_RES  4095.0f
-#define RL       10.0f
-#define RO       10.0f
+#define VREF     3.3f
+#define ADC_RES  4095.0f   // 12-bit ADC
+#define RL       10000.0f  // 10 kΩ
+#define RO       1000.0f  // 24 kΩ (calibrated in clean air)
+#define A        565.0f
+#define B        -1.48f
 
-#define A       -667.2f
-#define B        1334f
+// LPG(A=565,B=-1.48), CH4(A=393,B=-1.18), CO(A=472,B=-1.28), H2(A=575,B=-1.26)
 
 
 
@@ -27,5 +28,6 @@ float MQ2_PPM_OP(void) {
 	 float ppm = powf(10, (log10f(ratio) - B) / A);
    return ppm;
 }
+
 
 
